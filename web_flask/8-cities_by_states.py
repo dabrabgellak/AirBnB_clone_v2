a14bc39f90440e3fe85a7a1513268519a81906b1
dabrabgellak <barbaracalle10i3@gmail.com>
2020-01-22 21:38:02 +0000

    Done

---
+++ web_flask/8-cities_by_states.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+from models import storage
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.teardown_appcontext
+def tear_down_app_context():
+    storage.close()
+
+
+@app.route('/cities_by_states', strict_slashes=False)
+def states_list():
+    all_states = storage.all(state)
+    return render_template('8-cities_by_states.html', all_states=all_states)
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
